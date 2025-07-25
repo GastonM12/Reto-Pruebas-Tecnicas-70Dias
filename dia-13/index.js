77cb2b663896be9cd4795ff0ee9f10d106725417
GastonM12 <gaston_mori_96@hotmail.com>
2025-07-25 17:41:33 -0300

    Dia 13

---
+++ dia-13/index.js
@@ -0,0 +1,27 @@
+/*
+Enunciado Ejercicio 13:
+Crea una función que calcule el área de un cuadrado, un rectángulo o un triángulo
+ 
+Ejemplos:
+calcularAreaPoligono({ tipo: 'triangulo', base: 6, altura: 9 }); 
+ 
+Entrada: 27
+ 
+*/
+
+//crear una funcion que calcule el area de un triangulo o de un cuadrado 
+
+const calcularAreaPoligono=(poligono)=>{
+  //verificar si es un triangulo o un cuadrado 
+  if(poligono.tipo === "triangulo"){
+    return poligono.base * poligono.altura / 2
+  }
+  else if (poligono.tipo === "cuadrado"){
+    return poligono.base * poligono.altura
+  }
+  else{
+    return "No es un triangulo ni tampoco un cuadrado"
+  }
+}
+
+console.log(calcularAreaPoligono({ tipo: 'triangulo', base: 6, altura: 9 }));
